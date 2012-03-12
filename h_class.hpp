f8437f4c7885311cbcc843bf9a0228ad7c9840ba
foo <maciek.pacut@gmail.com>
2012-03-12 16:58:42 +0100

    Split hetmani into two files - declaration and implementation.

--- h_class.hpp
+++ h_class.hpp
@@ -30,74 +30,9 @@ public :
   void show();
 
   int dest_y;
-
 };
 
-
-hetman::hetman()
-{
-  //Initialize the offsets
-  x = 0;
-  y = 0;
-
-  //Initialize the velocity
-  xVel = 0;
-  yVel = 0;
-}
-
-
-void hetman::show()
-{
-  apply_surface( x, y, hetmans[n].queen, screen );
-}
-
-void hetman::move()
-{
-  //Move the queen left or right
-  x += xVel;
-
-  //If the queen went too far to the left or right
-  if( ( x < 0 ) || ( x + hetman_width > SCREEN_WIDTH ) )
-  {
-    //move back
-    x -= xVel;
-  }
-
-  //Move the queen up or down
-  y += yVel;
-
-  //If the queen went too far up or down
-  if( ( y < 0 ) || ( y + hetman_height > SCREEN_HEIGHT ) )
-  {
-    //move back
-    y -= yVel;
-  }
-}
-
-
-bool kolizja(int n)
-{
-  bool p = false;
-  for( int i = 0 ; i < n ; i++ )
-  {
-    if( hetmans[i].dest_y == hetmans[n].dest_y || abs( hetmans[n].dest_y - hetmans[i].dest_y ) == n - i )
-      p = true;
-  }
-  return p;
-}
-
-
-void ustaw(int n)
-{
-  for( int i = 0 ; i < 8 ; i++ )
-  {
-    hetmans[n].dest_y = i;
-    if( !kolizja(n) )
-      if( n < 7 )
-        ustaw(n+1);
-      else
-        game_loop();
-  }
-}
+bool kolizja(int n);
+void ustaw(int n);
 
 #endif
