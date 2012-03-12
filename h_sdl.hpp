8a510569bd1460d56014dd1bbcd588ad7057475d
foo <maciek.pacut@gmail.com>
2012-03-12 17:33:54 +0100

    Adjust init type.

--- h_sdl.hpp
+++ h_sdl.hpp
@@ -9,7 +9,7 @@
 
 SDL_Surface *load_image( std::string filename );
 void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination );
-void init();
+SDL_Surface *init(int width, int height, int bpp);
 void load_files();
 void clean_up();
 void game_loop();
