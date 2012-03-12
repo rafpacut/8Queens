0bffea6bf770b291b5c7bce3a9dac4f809bdd382
foo <maciek.pacut@gmail.com>
2012-03-12 17:27:00 +0100

    Move hetman's logic into main module. SDL module should not depend on program's logic.

--- h_sdl.cpp
+++ h_sdl.cpp
@@ -9,10 +9,6 @@ SDL_Surface *screen = NULL;
 const int SCREEN_WIDTH = 950;
 const int SCREEN_HEIGHT = 770;
 const int SCREEN_BPP = 32;
-//numer hetmana
-int n = 0;
-//vector of our hetmans
-vector<hetman> hetmans( 8, hetman() );
 
 //structure holding events
 SDL_Event event;
@@ -82,87 +78,4 @@ void init()
   //Set the window caption
   SDL_WM_SetCaption( "Osmiu hetmanow", NULL );
 
-}
-
-void load_files()
-{
-  background = load_image( "szachownica.jpg" );
-  for( int i = 0 ; i < 8 ; i++ )
-  {
-    hetmans[i].rect = load_image( "rect.jpg" );
-    hetmans[i].queen = load_image( "Queen.jpg" );
-  }
-}
-
-void clean_up()
-{
-  //Free the surfaces
-  SDL_FreeSurface( background );
-  for( int i = 0 ; i < 8 ; i++ )
-  {
-    SDL_FreeSurface( hetmans[i].rect );
-    SDL_FreeSurface( hetmans[i].queen );
-  }
-
-  //Quit SDL
-  SDL_Quit();
-}
-
-
-
-
-void game_loop()
-{
-  bool quit = false;
-  while( quit == false )
-  {
-    while( SDL_PollEvent( &event ) )
-    {
-      if( event.type == SDL_QUIT )
-      {
-        quit = true;
-      }
-      if(event.type == SDL_KEYDOWN)
-      {
-        SDLKey keyPressed = event.key.keysym.sym;
-        switch(keyPressed)
-        {
-          case SDLK_RETURN:
-            if(n == 7)
-            {hetman(); n= 0;
-              break;}
-            n++;break;
-          case SDLK_ESCAPE:
-            SDL_Quit();
-          case SDLK_UP: hetmans[n].yVel -= 5; break;
-          case SDLK_DOWN: hetmans[n].yVel += 5; break;
-          case SDLK_LEFT: hetmans[n].xVel -= 5; break;
-          case SDLK_RIGHT: hetmans[n].xVel += 5; break;
-
-        }
-      }
-      //If a key was released
-      else if( event.type == SDL_KEYUP )
-      {
-        //Adjust the velocity
-        switch( event.key.keysym.sym )
-        {
-          case SDLK_UP: hetmans[n].yVel += 5; break;
-          case SDLK_DOWN: hetmans[n].yVel -= 5; break;
-          case SDLK_LEFT: hetmans[n].xVel += 5; break;
-          case SDLK_RIGHT:hetmans[n]. xVel -= 5; break;
-        }
-      }
-    }
-    hetmans[n].move();
-    apply_surface( (n*118.75), (hetmans[n].dest_y*96.25), hetmans[n].rect, screen );
-    for( int i = 0 ; i <= n ; i++ )
-    {
-      hetmans[i].show();
-    }
-    //Update the screen
-    SDL_Flip( screen );
-    //Apply the background to the screen
-    apply_surface( 0, 0, background, screen );
-  }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,94 @@
 #include "h_class.hpp"
 #include "h_sdl.cpp"
 
+//numer hetmana
+int n = 0;
+//vector of our hetmans
+vector<hetman> hetmans( 8, hetman() );
+
+void load_files()
+{
+  background = load_image( "szachownica.jpg" );
+  for( int i = 0 ; i < 8 ; i++ )
+  {
+    hetmans[i].rect = load_image( "rect.jpg" );
+    hetmans[i].queen = load_image( "Queen.jpg" );
+  }
+}
+
+void clean_up()
+{
+  //Free the surfaces
+  SDL_FreeSurface( background );
+  for( int i = 0 ; i < 8 ; i++ )
+  {
+    SDL_FreeSurface( hetmans[i].rect );
+    SDL_FreeSurface( hetmans[i].queen );
+  }
+
+  //Quit SDL
+  SDL_Quit();
+}
+
+
+
+
+void game_loop()
+{
+  bool quit = false;
+  while( quit == false )
+  {
+    while( SDL_PollEvent( &event ) )
+    {
+      if( event.type == SDL_QUIT )
+      {
+        quit = true;
+      }
+      if(event.type == SDL_KEYDOWN)
+      {
+        SDLKey keyPressed = event.key.keysym.sym;
+        switch(keyPressed)
+        {
+          case SDLK_RETURN:
+            if(n == 7)
+            {hetman(); n= 0;
+              break;}
+            n++;break;
+          case SDLK_ESCAPE:
+            SDL_Quit();
+          case SDLK_UP: hetmans[n].yVel -= 5; break;
+          case SDLK_DOWN: hetmans[n].yVel += 5; break;
+          case SDLK_LEFT: hetmans[n].xVel -= 5; break;
+          case SDLK_RIGHT: hetmans[n].xVel += 5; break;
+
+        }
+      }
+      //If a key was released
+      else if( event.type == SDL_KEYUP )
+      {
+        //Adjust the velocity
+        switch( event.key.keysym.sym )
+        {
+          case SDLK_UP: hetmans[n].yVel += 5; break;
+          case SDLK_DOWN: hetmans[n].yVel -= 5; break;
+          case SDLK_LEFT: hetmans[n].xVel += 5; break;
+          case SDLK_RIGHT:hetmans[n]. xVel -= 5; break;
+        }
+      }
+    }
+    hetmans[n].move();
+    apply_surface( (n*118.75), (hetmans[n].dest_y*96.25), hetmans[n].rect, screen );
+    for( int i = 0 ; i <= n ; i++ )
+    {
+      hetmans[i].show();
+    }
+    //Update the screen
+    SDL_Flip( screen );
+    //Apply the background to the screen
+    apply_surface( 0, 0, background, screen );
+  }
+}
+
 int main( int argc, char* args[] )
 {
   init();
